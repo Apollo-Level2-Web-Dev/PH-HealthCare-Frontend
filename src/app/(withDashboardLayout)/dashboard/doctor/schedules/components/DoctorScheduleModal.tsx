5cdb0000128f9d4eb0630507b2e9a5675a716208
mizanmahi <mizanmahi24@gmail.com>
2024-04-23 20:51:17 +0600

    refactor: Mutliple select field with chip

--- src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
@@ -1,15 +1,56 @@
-import PHModal from "@/components/Shared/PHModal/PHModal";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import PHModal from '@/components/Shared/PHModal/PHModal';
+import dayjs, { Dayjs } from 'dayjs';
+import { useState } from 'react';
+import { useGetAllSchedulesQuery } from '@/redux/api/scheduleApi';
+import MultipleSelectFieldChip from './MultipleSelectFieldChip';
+
 type TProps = {
-  open: boolean;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+   open: boolean;
+   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
-  return (
-    <PHModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
-      <h1>ABC</h1>
-    </PHModal>
-  );
+   const [selectedDate, setSelectedDate] = useState(
+      dayjs(new Date()).toISOString()
+   );
+
+   const [selectedScheduleIds, setSelectedScheduleIds] = useState();
+
+   const query: Record<string, any> = {};
+
+   if (!!selectedDate) {
+      query['startDate'] = dayjs(selectedDate)
+         .hour(0)
+         .minute(0)
+         .millisecond(0)
+         .toISOString();
+      query['endDate'] = dayjs(selectedDate)
+         .hour(23)
+         .minute(59)
+         .millisecond(999)
+         .toISOString();
+   }
+
+   const { data, isLoading } = useGetAllSchedulesQuery(query);
+   const schedules = data?.schedules;
+
+   return (
+      <PHModal open={open} setOpen={setOpen} title='Create Doctor Schedule'>
+         <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+               label='Controlled picker'
+               value={dayjs(selectedDate)}
+               onChange={(newValue) =>
+                  setSelectedDate(dayjs(newValue).toISOString())
+               }
+            />
+         </LocalizationProvider>
+         <MultipleSelectFieldChip schedules={schedules} />
+      </PHModal>
+   );
 };
 
 export default DoctorScheduleModal;
--- src/app/(withDashboardLayout)/dashboard/doctor/schedules/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedules/page.tsx
@@ -1,18 +1,19 @@
-"use client";
-import { Box, Button } from "@mui/material";
-import DoctorScheduleModal from "./components/DoctorScheduleModal";
-import { useState } from "react";
+'use client';
+import { Box, Button } from '@mui/material';
+import DoctorScheduleModal from './components/DoctorScheduleModal';
+import { useState } from 'react';
 
 const DoctorSchedulesPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  return (
-    <Box>
-      <Button onClick={() => setIsModalOpen(true)}>
-        Create Doctor Schedule
-      </Button>
-      <DoctorScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
-    </Box>
-  );
+   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+   return (
+      <Box>
+         <Button onClick={() => setIsModalOpen(true)}>
+            Create Doctor Schedule
+         </Button>
+         <DoctorScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
+         <Box>Schedule table goes here</Box>
+      </Box>
+   );
 };
 
 export default DoctorSchedulesPage;
